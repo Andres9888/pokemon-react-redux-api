215ebdb39986c1c63966910aae9ecf03f8713f49
Andres9888 <Andres9888@Gmail.com>
2022-06-25 14:43:15 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
-import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import React, { useCallback, useState, useEffect } from "react"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
 
-import fetchPokemonAction from "./fetchpokemon";
+import fetchPokemonAction from "./fetchpokemon"
 import {
   getPokemonError,
   getPokemon,
-  getPokemonLoading
-} from "./reducers/reducers";
+  getPokemonLoading,
+} from "./reducers/reducers"
 
-import PokemonList from "../src/components/container/PokemonList";
-import _ from "lodash";
-import "./App.css";
+import PokemonList from "../src/components/container/PokemonList"
+import "./App.css"
 
-class PokeApp extends Component {
-  increment = () => {
-    this.setState({ species: { ...this.state.species, count: 1 } });
-  };
-
-  componentWillMount() {
-    const { fetchPokemon } = this.props;
-    fetchPokemon();
+const PokeApp = (props) => {
+  const [species, setSpecies] = useState()
+  const increment = () => {
+    setSpecies({ ...species, count: 1 })
   }
-
-  render() {
-    const { pokemon, error, loading } = this.props;
-
-    return (
+  useEffect(() => {
+    props.fetchPokemon()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div className="App">
       <div className="pokeapp">
         <h1> The Kanto PokeDex! </h1>
-        <button onClick={this.increment}></button>
-        <PokemonList data={this.props} increment={this.increment} />
+        <button onClick={increment}></button>
+        <PokemonList data={props} increment={increment} />
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   error: getPokemonError(state),
   pokemon: getPokemon(state),
-  loading: getPokemonLoading(state)
-});
+  loading: getPokemonLoading(state),
+})
 
-const mapDispatchToProps = dispatch =>
+const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
     {
-      fetchPokemon: fetchPokemonAction
+      fetchPokemon: fetchPokemonAction,
     },
     dispatch
-  );
+  )
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PokeApp);
+export default connect(mapStateToProps, mapDispatchToProps)(PokeApp)
--- src/fetchpokemon.js
+++ src/fetchpokemon.js
@@ -1,23 +1,21 @@
-import {fetchPokemonLoading, fetchPokemonSuccess, fetchPokemonError} from './actions/actions';
-
-    
+import {
+  fetchPokemonLoading,
+  fetchPokemonSuccess,
+  fetchPokemonError,
+} from "./actions/actions"
+import axios from "axios"
 function fetchPokemon() {
-    return dispatch => {
-        dispatch(fetchPokemonLoading());
-        fetch("http://pokeapi.co/api/v2/pokemon?limit=151")
-        .then(res => res.json())
-        .then(res => {
-            if(res.error) {
-                throw(res.error);
-            }
-            dispatch(fetchPokemonSuccess(res.results));
-            return res.results;
-
-        })
-        .catch(error => {
-            dispatch(fetchPokemonError(error));
-        })
+  return async (dispatch) => {
+    dispatch(fetchPokemonLoading())
+    try {
+      const url = "https://pokeapi.co/api/v2/pokemon/?limit=151"
+      const { data } = await axios.get(url)
+      dispatch(fetchPokemonSuccess(data.results))
+    } catch (error) {
+      dispatch(fetchPokemonError(error))
+      throw error
     }
+  }
 }
 
-export default fetchPokemon;
+export default fetchPokemon
